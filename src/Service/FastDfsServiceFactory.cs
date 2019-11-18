ea7709cd5b95c9deb1a4d3d01d060e4f230f32f4
kasimlz <liuzhe_kasim@outlook.com>
2019-11-18 08:58:23 +0800

    add public service factory

---
+++ src/Service/FastDfsServiceFactory.cs
@@ -0,0 +1,12 @@
+﻿using GoFastDFS.Client.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoFastDFS.Client
+{
+	public static class FastDfsServiceFactory
+	{
+		public static IFastDFSService GetService() => DependenceInjectService.GetService<IFastDFSService>();
+	}
+}
